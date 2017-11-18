870ef253d5ef9c4aecd6b609a22888b967feca3d
Jennifer Haddock <jentx123@hotmail.com>
2017-11-18 05:06:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file.
 function lowerCaseDrivers(array){
-  const newArray = map(array, function (element) { return element.toLowerCase(); });
+  const newArray = array.map(obj.toLowerCase(), obj);
 }
