f3d8e5c34045768d683b5960387df4f3339e17e0
Jennifer Haddock <jentx123@hotmail.com>
2017-11-18 05:12:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function lowerCaseDrivers(array){
   return array.map(drivername => drivername.toLowerCase())
 }
+
+function nameToAttributes(array){
+  return array.map(function(name){
+    return {firstName: name.split(' ')[0], lastName: name.split(' ')[1]}
+  }  
+}
